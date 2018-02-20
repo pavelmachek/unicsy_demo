5c2a7d7f9dacbb9ca56c3bc6374c937593b5e850
Pavel <pavel@ucw.cz>
2018-02-20 10:46:53 +0100

    Refactor lockd to make proper locking feasible.

--- hacks/lockd.py
+++ hacks/lockd.py
@@ -9,43 +9,22 @@ import os
 import subprocess
 import watchdog
 
-class TtyLocker:
+class Locker:
     def __init__(m):
         m.wd = watchdog.Watchdog("screen")
-        m.lock_tty = 8
-        m.l_tty = "/dev/tty%d" % m.lock_tty
-        # 2 works for postmarketos
-        m.x_tty = 7
-        os.system("sudo chmod 666 "+m.l_tty)
 
     def screen_off(m):
-        print("Screensaver activated")
-        os.system("sudo chvt %d" % m.lock_tty)
-        open(m.l_tty, "w").write("Screen locked")
-        os.system("date > "+m.l_tty)
         os.system("echo 1 > /sys/kernel/debug/pm_debug/enable_off_mode")
-        os.system("TERM=linux setterm -blank force < %s > %s" % (m.l_tty, m.l_tty))
-        os.system("TERM=linux sudo setterm -blank force < %s > %s" % (m.l_tty, m.l_tty))
         m.wd.write("off", "")
 
     def screen_on(m, complete = True):
         os.system("echo 0 > /sys/kernel/debug/pm_debug/enable_off_mode")
-        os.system("sudo chvt %d" % m.x_tty)
         m.wd.write("on", "")
-        if complete:
-            os.system("xscreensaver-command -deactivate")
 
     def on_screensaver(m):
         m.screen_off()
 
-    def run_unlock(m):
-        while True:
-            print("Waiting for enter")
-            os.system("read A < %s" % m.l_tty)
-            print("Got enter")
-            m.screen_on()
-
-    def run(m):
+    def run_loop(m):
         events = subprocess.Popen(["xscreensaver-command", "-watch"], bufsize=1, stdout=subprocess.PIPE)
         while True:
             l = events.stdout.readline()
@@ -61,11 +40,45 @@ class TtyLocker:
             else:
                 print("Got unknown event, ", split[0])
 
+class TtyLocker(Locker):
+    def __init__(m):
+        Locker.__init__(m)
+        m.lock_tty = 8
+        m.l_tty = "/dev/tty%d" % m.lock_tty
+        # 2 works for postmarketos
+        m.x_tty = 7
+        os.system("sudo chmod 666 "+m.l_tty)
+
+    def screen_off(m):
+        print("Screensppppaver activated")
+        os.system("sudo chvt %d" % m.lock_tty)
+        open(m.l_tty, "w").write("Screen locked")
+        os.system("date > "+m.l_tty)
+        os.system("TERM=linux setterm -blank force < %s > %s" % (m.l_tty, m.l_tty))
+        os.system("TERM=linux sudo setterm -blank force < %s > %s" % (m.l_tty, m.l_tty))
+        Locker.screen_off(m)
+
+    def screen_on(m, complete = True):
+        Locker.screen_on(m)
+        os.system("sudo chvt %d" % m.x_tty)
+        if complete:
+            os.system("xscreensaver-command -deactivate")
+
+    def run_unlock(m):
+        while True:
+            print("Waiting for enter")
+            os.system("read A < %s" % m.l_tty)
+            print("Got enter")
+            m.screen_on()
+
+    def run(m):
+        if os.fork():
+            l.run_loop()
+        else:
+            l.run_unlock()
+
+
 if __name__ == "__main__":
     l = TtyLocker()
-    if os.fork():
-        l.run()
-    else:
-        l.run_unlock()
-
+    l.run()
     #l.screen_off()
